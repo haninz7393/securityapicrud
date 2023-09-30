a60cbe0af41f6245883a6d54055643218592bf55
haninz7393 <haninz7393@gmail.com>
2023-10-01 00:52:10 +0300

    first commit

---
+++ src/main/java/com/sol/securityapicrud/dto/SignUpDto.java
@@ -0,0 +1,12 @@
+package com.sol.securityapicrud.dto;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+}
---
+++ src/test/java/com/sol/securityapicrud/SecurityapicrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sol.securityapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityapicrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
